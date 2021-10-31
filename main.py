8dd083a070682d7395387050fbcb76b828df6724
Vinh <73513296+VinnyBui@users.noreply.github.com>
2021-10-30 22:22:51 -0700

    Ability to react to text

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import discord
-import random
+import os
 
 TOKEN = 'ODgyMTA5Njc2MTE5MDExMzM4.YS2mpw.DVn5838VjRjZ8qfdcWmZU0_bFzU'
+#TOKEN = os.getenv("CODE")
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
-  print('Sup bitches {0.user}.'.format(client))
+  print('Online! {0.user}.'.format(client))
+
+
+@client.event
+async def on_message(message):
+  username = str(message.author).split('#')[0]
+  user_message = str(message.content)
+  channel = str(message.channel.name)
+  print(f'{username}: {user_message} ({channel})')
+
+  if message.author == client.user:
+    await message.add_reaction('✅')
+    await message.add_reaction('❌')
+    return
+
+
+  if message.channel.name == 'testing':
+    if user_message.startswith('!vote'):
+      msg = user_message.split('!vote')[1]
+      await message.channel.send(f'```**Voting on{msg}**```')
+
+  if message.content == "!end":
+    await client.close()
+
+
 
 client.run(TOKEN)
