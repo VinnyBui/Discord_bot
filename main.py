7c334a0bad05ff4f0f78f758be406048fa48ae23
Vinh <73513296+VinnyBui@users.noreply.github.com>
2021-10-31 00:28:45 -0700

    Got env variable to work

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-TOKEN = 'ODgyMTA5Njc2MTE5MDExMzM4.YS2mpw.DVn5838VjRjZ8qfdcWmZU0_bFzU'
+TOKEN=os.getenv('SECRETKEY')
 
 client = discord.Client()
 @client.event
 async def on_ready():
   print('Online! {0.user}.'.format(client))
-
 
 @client.event
 async def on_message(message):
